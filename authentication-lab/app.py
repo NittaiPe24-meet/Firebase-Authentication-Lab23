62acac51030f51fe7475e30d66664ce783e85a71
NittaiPe24-meet <nittai.peri@gmail.com>
2023-07-24 12:11:55 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 
+
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
+firebaseConfig = {
+  "apiKey": "AIzaSyDRGjO3hLbaGuUzNjjoUxEuw16Xwadkr7I",
+  "authDomain": "yello-ed04e.firebaseapp.com",
+  "projectId": "yello-ed04e",
+  "storageBucket": "yello-ed04e.appspot.com",
+  "messagingSenderId": "29543276179",
+  "appId": "1:29543276179:web:10a8c74944ae275127f766",
+  "measurementId": "G-QZ9QW2LPND",
+  "databaseURL": ""
+}
+
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth
 
 @app.route('/', methods=['GET', 'POST'])
 def signin():
@@ -13,6 +28,15 @@ def signin():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    error = ""
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            login_session['user'] = auth.create_user_with_email_and_password(email, password)
+           return redirect(url_for('add_tweet'))
+        except:
+           error = "Authentication failed"
     return render_template("signup.html")
 
 
