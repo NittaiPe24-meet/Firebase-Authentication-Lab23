c49add40628936910d0cce4cc9a529e4fcc64d6d
NittaiPe24-meet <nittai.peri@gmail.com>
2023-07-24 19:53:01 +0300

    nittai

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -15,14 +15,14 @@ config = {
   "appId": "1:29543276179:web:10a8c74944ae275127f766",
   "measurementId": "G-QZ9QW2LPND",
   "databaseURL": "https://yello-ed04e-default-rtdb.firebaseio.com"
-}
+};
 
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
 
-
+db.get().val()
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
@@ -32,19 +32,24 @@ def signin():
 def signup():
     error = ""
     if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
+        user = {"email":(request.form['email']), "password":(request.form['password']), "name":(request.form['name']), "username":(request.form['username'])};
+        db.child("Users").child(UID).set(user)
+        return user
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
-           return redirect(url_for('add_tweet'))
+            return redirect(url_for('add_tweet'))
         except:
-           error = "Authentication failed"
+            error = "Authentication failed"
     return render_template("signup.html")
 
 
 @app.route('/add_tweet', methods=['GET', 'POST'])
-def add_tweet():methods
-    return render_template("add_tweet.html")
+def add_tweet():
+    if request.method == 'POST':
+        tweet = {"title":(request.form['title']), "content":(request.form['content']), "uid":(firebase.auth().user.uid)};
+        db.child("Tweets").push(tweet)
+    else:
+        return render_template("add_tweet.html")
 
 
 if __name__ == '__main__':
